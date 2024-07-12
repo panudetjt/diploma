6392e139628d43552fe8cb22d0fd779bbc965502
Panudet Tammwongsa <3482285+panudetjt@users.noreply.github.com>
2024-07-13 02:40:19 +0700

    feat: remove dead code and update image editor to use certification image

--- src/components/image-editor/image-editor.tsx
+++ src/components/image-editor/image-editor.tsx
@@ -1,11 +1,10 @@
 "use client"
-import React, { useState, useRef, useEffect, ChangeEvent, useCallback } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
-import diploma from './diploma.png'
+import diploma from './certification.png'
 import localFont from 'next/font/local'
 import { Canvg } from 'canvg';
 import { ModeToggle } from '../mode-toggle';
@@ -32,77 +31,6 @@ export function ImageEditor() {
         img.src = src;
     }
 
-    // const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    //     const files = e.target.files;
-    //     if (!files) return;
-    //     const file = files[0];
-    //     const reader = new FileReader();
-
-    //     reader.onload = (event) => {
-    //         const result = event.target?.result;
-    //         if (typeof result !== "string") return
-    //         loadImage(result);
-
-    //     };
-
-    //     reader.readAsDataURL(file);
-    // };
-
-
-    // const drawImage = useCallback(() => {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas) return;
-    //     const svg = svgRef.current;
-    //     if (!svg) return;
-    //     if (!image) return;
-    //     const ctx = canvas.getContext('2d');
-    //     if (!ctx) return;
-    //     // canvas.width = image.width;
-    //     // canvas.height = image.height;
-    //     canvas.width = svgSize.width;
-    //     canvas.height = svgSize.height;
-
-    //     ctx.drawImage(image, 0, 0);
-
-    //     // if (text) {
-    //     //     const centerX = canvas.width / 2;
-    //     //     const centerY = canvas.height / 2;
-    //     //     ctx.font = `${fontSize}px ${fontFamily}`;
-    //     //     ctx.fillStyle = fontColor;
-    //     //     ctx.textAlign = 'center';
-    //     //     ctx.textBaseline = 'middle';
-    //     //     ctx.fillText(text, centerX, centerY);
-    //     // }
-    //     // }, [fontColor, fontFamily, fontSize, image, text]);
-    // }, [image, svgSize.height, svgSize.width]);
-
-    // const applyGrayscale = () => {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas) return;
-    //     const ctx = canvas.getContext('2d');
-    //     if (!ctx) return;
-    //     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    //     const data = imageData.data;
-
-    //     for (let i = 0; i < data.length; i += 4) {
-    //         const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-    //         data[i] = avg;     // Red
-    //         data[i + 1] = avg; // Green
-    //         data[i + 2] = avg; // Blue
-    //     }
-
-    //     ctx.putImageData(imageData, 0, 0);
-    // };
-
-    // const saveImage = () => {
-    //     const canvas = canvasRef.current;
-    //     if (!canvas) return;
-    //     const link = document.createElement('a');
-    //     link.download = 'edited_image.png';
-    //     link.href = canvas.toDataURL();
-    //     link.click();
-    // };
-    // 
     const saveImage = async () => {
         const svg = svgRef.current;
         if (!svg) return;
@@ -141,46 +69,15 @@ export function ImageEditor() {
                 <ModeToggle />
             </CardHeader>
             <CardContent>
-                {/* <img src={diploma.blurDataURL} alt="diploma" /> */}
                 <div className="space-y-4">
-                    {/* <div>
-                        <Label htmlFor="imageUpload">Upload Image</Label>
-                        <Input id="imageUpload" type="file" accept="image/*" onChange={handleImageUpload} />
-                    </div> */}
                     <div>
                         <Label htmlFor="textInput1">Text 1</Label>
                         <Input id="textInput1" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text 1" />
                         <Label htmlFor="textInput2">Text 2</Label>
                         <Input id="textInput2" type="text" value={text2} onChange={(e) => setText2(e.target.value)} placeholder="Enter text 2" />
                     </div>
-                    {/* <div className="grid grid-cols-3 gap-4">
-                        <div>
-                            <Label htmlFor="fontFamily">Font Family</Label>
-                            <Select value={fontFamily} onValueChange={setFontFamily}>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select font" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem value="Arial">Arial</SelectItem>
-                                    <SelectItem value="Verdana">Verdana</SelectItem>
-                                    <SelectItem value="Times New Roman">Times New Roman</SelectItem>
-                                    <SelectItem value="Courier">Courier</SelectItem>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <div>
-                            <Label htmlFor="fontSize">Font Size</Label>
-                            <Input id="fontSize" type="number" value={fontSize} onChange={(e) => setFontSize(Number(e.target.value))} min="1" max="100" />
-                        </div>
-                        <div>
-                            <Label htmlFor="fontColor">Font Color</Label>
-                            <Input id="fontColor" type="color" value={fontColor} onChange={(e) => setFontColor(e.target.value)} />
-                        </div>
-                    </div> */}
                     <svg
                         ref={svgRef}
-                        // width={svgSize.width}
-                        // height={svgSize.height}
                         width="100%"
                         viewBox={`0 0 ${svgSize.width} ${svgSize.height}`}
                         className="w-full border border-gray-300"
@@ -219,7 +116,6 @@ export function ImageEditor() {
             </CardContent>
             <CardFooter className="justify-center">
                 <div className="flex space-x-2">
-                    {/* <Button onClick={applyGrayscale} disabled={!image}>Apply Grayscale</Button> */}
                     <Button onClick={saveImage} disabled={!image}>Save Image</Button>
                 </div>
             </CardFooter>
@@ -228,7 +124,7 @@ export function ImageEditor() {
 };
 
 function getDataUrl(src: string) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const img = new Image();
         img.onload = () => {
             const canvas = document.createElement('canvas');
